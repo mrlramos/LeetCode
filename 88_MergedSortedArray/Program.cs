243d88c315609849ea02b430724fe90940f53124
Marlon Ramos <marlon_raoni10@hotmail.com>
2023-10-01 14:32:38 -0300

    88 - Merged Sorted Array

---
+++ 88_MergedSortedArray/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Merge(new int[] { 1, 2, 3, 0, 0, 0 }, 3, new int[] { 2, 5, 6 }, 3);
+
+static void Merge(int[] nums1, int m, int[] nums2, int n)
+{
+    int p1 = m - 1;
+    int p2 = n - 1;
+    int i = m + n - 1;
+
+    while (p2 >= 0)
+    {
+        if (p1 >= 0 && nums1[p1] > nums2[p2])
+        {
+            nums1[i--] = nums1[p1--];
+        }
+        else
+        {
+            nums1[i--] = nums2[p2--];
+        }
+    }
+
+    Console.WriteLine($"The arrays we are merging are " +
+            $"[{string.Join(", ", new int[] { 1, 2, 3, 0, 0, 0 })}] and [{string.Join(", ", new int[] { 2, 5, 6 })}]." +
+            $"\r\nThe result of the merge is [{string.Join(", ", nums1)}] with " +
+            $"the underlined elements coming from nums1.");
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # LeetCode
 Repository to keep the code made on website LeetCode
+
+88 - Merged Sorted Array
