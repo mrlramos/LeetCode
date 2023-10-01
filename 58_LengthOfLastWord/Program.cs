ceeb949939913b2cddfd6a22c8aea0662f1fdf8e
Marlon Ramos <marlon_raoni10@hotmail.com>
2023-10-01 17:22:42 -0300

    58 - Length of Last Word

---
+++ 58_LengthOfLastWord/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine(LengthOfLastWord("s"));
+
+static int LengthOfLastWord(string s)
+{
+    var noSpaceEnd = s.TrimEnd();
+    int lastSpaceIndex = noSpaceEnd.LastIndexOf(' ');
+
+    if (lastSpaceIndex == -1)
+        return noSpaceEnd.Length;
+
+    return noSpaceEnd.Length - lastSpaceIndex - 1;
+}
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # LeetCode
-Repository to keep the code made on website LeetCode
+Repository to keep the code made on website LeetCode. Version: ".NET 6" 
 
 88 - Merged Sorted Array
+
+58 - Length of Last Word
